b03146da018f71bf7010de3fb36532de664c542b
amit_1 <amit@stillwrinkled.com>
2023-05-19 20:03:39 +0530

    OpenAI integration complete

--- app.py
+++ app.py
@@ -1,41 +1,43 @@
-# app.py
-
-from flask import Flask, request, render_template
+import os
 import openai
-
-openai.api_key = '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'
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+openai.api_key = "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"  # Replace with your OpenAI API key
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        employer = request.form.get('employer')
-        role = request.form.get('role')
-        coverLetterParas = request.form.get('coverLetterParas')
-        jd = request.form.get('jd')
-        cv = request.form.get('cv')
-        desc1 = request.form.get('desc1')
-        desc2 = request.form.get('desc2')
-        desc3 = request.form.get('desc3')
-        desc4 = request.form.get('desc4')
-        desc5 = request.form.get('desc5')
+    if request.method == "POST":
+        employer = request.form.get("employer")
+        role = request.form.get("role")
+        cover_letter_paras = request.form.get("coverLetterParas")
+        jd = request.form.get("jd")
+        desc1 = request.form.get("desc1")
+        desc2 = request.form.get("desc2")
+        desc3 = request.form.get("desc3")
+        desc4 = request.form.get("desc4")
+        cv = request.form.get("cv")
 
-        # Construct the message to send to ChatGPT API
-        message = f"I am applying for the role of {role} at {employer}. Here are some details about me: {desc1}, {desc2}, {desc3}, {desc4}, {desc5}. The job description is as follows: {jd}. My CV is as follows: {cv}. Please generate a cover letter with {coverLetterParas} paragraphs."
+        # Construct the input prompt for the ChatGPT API
+        prompt = f"Employer: {employer}\nRole: {role}\nCover Letter Paragraphs: {cover_letter_paras}\nJob Description: {jd}\nDescription 1: {desc1}\nDescription 2: {desc2}\nDescription 3: {desc3}\nDescription 4: {desc4}\nCV:\n{cv}\n"
 
+        # Call the ChatGPT API to generate the cover letter
         response = openai.Completion.create(
-          engine="davinci-codex",
-          prompt=message,
-          temperature=0.5,
-          max_tokens=1000
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=500,  # Adjust the max tokens as needed
+            temperature=0.7,  # Adjust the temperature as needed
+            n=1,
+            stop=None,
+            echo=False
         )
 
-        output = response.choices[0].text.strip()
+        # Extract the generated cover letter from the API response
+        cover_letter = response.choices[0].text.strip()
 
-        return render_template('index.html', output=output)
+        return render_template("index.html", output=cover_letter)
     else:
-        return render_template('index.html')
+        return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
